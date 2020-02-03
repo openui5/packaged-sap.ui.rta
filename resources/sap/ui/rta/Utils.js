d81a78ab237eb9bc39e8ff37644037c870ee000b
OpenUI5 <openui5@sap.com>
2020-02-03 15:31:19 +0100

    1.52.38

--- resources/sap/ui/rta/Utils.js
+++ resources/sap/ui/rta/Utils.js
@@ -1,6 +1,6 @@
 /*!
  * UI development toolkit for HTML5 (OpenUI5)
- * (c) Copyright 2009-2019 SAP SE or an SAP affiliate company.
+ * (c) Copyright 2009-2020 SAP SE or an SAP affiliate company.
  * Licensed under the Apache License, Version 2.0 - see LICENSE.txt.
  */
 
@@ -27,7 +27,7 @@ function(
 	 * @class Utility functionality to work with controls, e.g. iterate through aggregations, find parents, etc.
 	 *
 	 * @author SAP SE
-	 * @version 1.52.37
+	 * @version 1.52.38
 	 *
 	 * @private
 	 * @static
--- resources/sap/ui/rta/plugin/additionalElements/AddElementsDialog.js
+++ resources/sap/ui/rta/plugin/additionalElements/AddElementsDialog.js
@@ -1,6 +1,6 @@
 /*!
  * UI development toolkit for HTML5 (OpenUI5)
- * (c) Copyright 2009-2019 SAP SE or an SAP affiliate company.
+ * (c) Copyright 2009-2020 SAP SE or an SAP affiliate company.
  * Licensed under the Apache License, Version 2.0 - see LICENSE.txt.
  */
 sap.ui.define([
@@ -58,7 +58,7 @@ sap.ui.define([
 	 * @class Context - Dialog for available Fields in Runtime Authoring
 	 * @extends sap.ui.base.ManagedObject
 	 * @author SAP SE
-	 * @version 1.52.37
+	 * @version 1.52.38
 	 * @constructor
 	 * @private
 	 * @since 1.44
